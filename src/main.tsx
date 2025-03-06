bedd79590ab98b8ea48ab0a7025168af1352f088
Leo Mancini <mancini@leo.gd>
2025-03-05 21:22:31 -0500

    Fixing imports

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
-import Kiosk from "./Springboard.tsx";
-import ControlPanel from "./ControlPanel.tsx";
+import Display from "./Display";
+import RemoteControl from "./RemoteControl";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Kiosk />} />
-        <Route path="/control" element={<ControlPanel />} />
+        <Route path="/" element={<Display />} />
+        <Route path="/remote-control" element={<RemoteControl />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
