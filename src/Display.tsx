a917354a611dc2bc3e9d01853f65bd813573d846
Leo Mancini <mancini@leo.gd>
2025-03-07 15:28:58 -0500

    Go back to other way of hiding cursor

--- src/Display.tsx
+++ src/Display.tsx
@@ -13,24 +13,6 @@ const Display = styled.div`
   }
 `;
 
-const CursorHider = styled.button`
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  z-index: 9999;
-  pointer-events: none;
-  cursor: none !important;
-  width: 100%;
-  height: 100%;
-  padding: 0;
-  margin: 0;
-  border: none;
-  background: transparent;
-  outline: none;
-`;
-
 const App = styled.iframe`
   width: 100%;
   height: 100%;
@@ -101,7 +83,6 @@ function SpringBoard() {
 
   return (
     <Display>
-      <CursorHider autoFocus />
       <StatusIndicator isConnected={isConnected} />
       <App
         src={currentUrl}
