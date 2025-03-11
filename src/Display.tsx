a6254b2741dc60c0a757dd5f96e88a71abd7c178
Leo Mancini <mancini@leo.gd>
2025-03-10 22:59:58 -0400

    Removing cursor hiding

--- src/Display.tsx
+++ src/Display.tsx
@@ -6,22 +6,12 @@ const Display = styled.div`
   height: 100vh;
   width: 100vw;
   position: relative;
-  cursor: none !important;
-
-  * {
-    cursor: none !important;
-  }
 `;
 
 const App = styled.iframe`
   width: 100%;
   height: 100%;
   border: none;
-  cursor: none !important;
-
-  * {
-    cursor: none !important;
-  }
 `;
 
 const StatusIndicator = styled.div<{ isConnected: boolean }>`
